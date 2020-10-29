c70980465586e531ae8b42b8b07189959272441e
Raphael Robles Ribeiro <ra_robles@hotmail.com>
2020-10-29 10:09:25 -0300

    Detalhes do Vendedor e Eager Loading

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Pages.Internal.Account;
+﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
@@ -29,7 +29,7 @@ namespace SalesWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
